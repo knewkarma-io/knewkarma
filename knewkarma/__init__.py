c7033d1bc09565260f26bd2fb30b5fe45f870d6c
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-10-25 21:53:52 +0200

    Update __init__.py

--- knewkarma/__init__.py
+++ knewkarma/__init__.py
@@ -2,5 +2,5 @@ __author__ = "Richard Mwewa"
 __about__ = "https://about.me/rly0nheart"
 __version__ = "1.5.0.0"
 __description__ = """# Knew Karma
-> A **Reddit** Data Analysis Toolkit."""
+> **Reddit** Data Analysis Toolkit."""
 __epilog__ = f"# by [{__author__}]({__about__})"
