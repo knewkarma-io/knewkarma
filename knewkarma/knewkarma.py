5c64e8a400b6eef4dd895686668cf5ddeccda9ae
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-11-06 01:19:24 +0200

    Update knewkarma.py

--- knewkarma/knewkarma.py
+++ knewkarma/knewkarma.py
@@ -1,9 +1,10 @@
 def on_call():
     from . import __version__
+    from .coreutils import datetime, log, path_finder
     from .executor import Executor
-    from .coreutils import arguments, datetime, log, path_finder
     from .masonry import Masonry
     from .messages import message
+    from .parser import create_parser
 
     print(
         """
@@ -11,45 +12,39 @@ def on_call():
 ┃┫ ┏┓┏┓┓┏┏  ┃┫ ┏┓┏┓┏┳┓┏┓
 ┛┗┛┛┗┗ ┗┻┛  ┛┗┛┗┻┛ ┛┗┗┗┻"""
     )
-
+    parser = create_parser()
     tree_masonry = Masonry()
     start_time = datetime.now()
-    executor = Executor(arguments=arguments, tree_masonry=tree_masonry)
 
-    path_finder()
-    try:
-        executor_title = (
-            "Command-Line Arguments"
-            if arguments.mode
-            else "Interactive Command-Line Wizard"
-        )
-        log.info(
-            message(
-                message_type="info",
-                message_key="program_started",
-                program_name=f"Knew Karma",
-                executor_title=executor_title,
-                program_version=__version__,
-                start_time=start_time.strftime("%a %b %d %Y, %H:%M:%S %p"),
+    if parser.parse_args().mode:
+        executor = Executor(arguments=parser.parse_args(), tree_masonry=tree_masonry)
+        path_finder()
+        try:
+            log.info(
+                message(
+                    message_type="info",
+                    message_key="program_started",
+                    program_name=f"Knew Karma",
+                    program_version=__version__,
+                    start_time=start_time.strftime("%a %b %d %Y, %H:%M:%S %p"),
+                )
             )
-        )
-        tree_masonry.api.check_updates()
-        if arguments.mode:
+            tree_masonry.api.check_updates()
             executor.execute_cli_arguments()
-        else:
-            executor.execute_cli_wizards()
-    except KeyboardInterrupt:
-        log.warning(
-            message(
-                message_type="warning",
-                message_key="user_interruption",
+        except KeyboardInterrupt:
+            log.warning(
+                message(
+                    message_type="warning",
+                    message_key="user_interruption",
+                )
             )
-        )
-    finally:
-        log.info(
-            message(
-                message_type="info",
-                message_key="program_stopped",
-                run_time=datetime.now() - start_time,
+        finally:
+            log.info(
+                message(
+                    message_type="info",
+                    message_key="program_stopped",
+                    run_time=datetime.now() - start_time,
+                )
             )
-        )
+    else:
+        parser.print_usage()
