300da8259b01b4c6c7b9b32d5b2d53085d379b12
rly0nheart <74001397+rly0nheart@users.noreply.github.com>
2024-07-22 02:56:01 +0200

    Small fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,8 +8,8 @@ project = "Knew Karma"
 author = "Richard Mwewa"
 copyright = f"{date.today().year}, {author}"
 
-release = "5.2"
-version = "5.2.0"
+release = "5.3.1"
+version = "5.3.1"
 
 # -- General configuration
 
