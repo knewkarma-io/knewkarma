889cbcee4589a509daefcc67415048538832d4fe
rly0nheart <74001397+rly0nheart@users.noreply.github.com>
2024-07-25 21:19:15 +0200

    Minor changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,8 +37,9 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_favicon = "images/favicon.ico"
+html_show_sphinx = False
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
