2f42ea5ca2792cee9b15b928715095d2ea2d93d3
rly0nheart <74001397+rly0nheart@users.noreply.github.com>
2024-07-25 20:55:11 +0200

    Minor changes

--- knewkarma/help.py
+++ knewkarma/help.py
@@ -17,9 +17,8 @@ class Help:
     summary: str = f"**Knew Karma**: *A Reddit Data Analysis Toolkit* — by {author}"
     description: str = """
 **Knew Karma** (/nuː ‘kɑːrmə/) is a Reddit data analysis toolkit designed to provide an extensive range of
-functionalities for exploring and analysing Reddit data. It includes a **Command-Line Interface (CLI)** (*Snap/PyPI
-Package*), and an
-**Application Programming Interface (API)** (*PyPI Package*) to enable an easy integration in other Python Projects.
+functionalities for exploring and analysing Reddit data. It includes a **Command-Line Interface (CLI)**, and an
+**Application Programming Interface (API)** to enable an easy integration in other Python Projects.
 """
 
     examples: dict = {
