bef9927b15007cac0c8a149603dadfbdae354165
rly0nheart <74001397+rly0nheart@users.noreply.github.com>
2024-08-10 03:15:40 +0200

    6.1.0

--- knewkarma/tools/__init__.py
+++ knewkarma/tools/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ["data_utils", "general_utils", "package_utils", "parsing_utils", "time_utils"]
+__all__ = [
+    "data_utils",
+    "general_utils",
+    "package_utils",
+    "parsing_utils",
+    "time_utils",
+]
