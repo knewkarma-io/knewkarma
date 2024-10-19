e4872a9c02c2fa538f0514cb24ed2bc2af25a1c6
rly0nheart <74001397+rly0nheart@users.noreply.github.com>
2024-10-19 12:20:22 +0200

    Applying fix for kraw.Connection in KRAW 0.2.3

--- src/knewkarma/meta/version.py
+++ src/knewkarma/meta/version.py
@@ -1,6 +1,6 @@
 class Version:
     major: tuple[int, str] = 7, "MAJOR"
     minor: tuple[int, str] = 0, "MINOR"
-    patch: tuple[int, str] = 11, "PATCH"
+    patch: tuple[int, str] = 12, "PATCH"
     full_version: str = f"{major[0]}.{minor[0]}.{patch[0]}"
     release: str = f"{major[0]}.{minor[0]}"
