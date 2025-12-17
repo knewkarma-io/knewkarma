13706c94db6b5ae45be24733ab75ce35fc90edf8
Ritchie Mwewa <74001397+rly0nheart@users.noreply.github.com>
2025-12-17 03:07:28 +0200

    Update version.py

--- src/knewkarma/meta/version.py
+++ src/knewkarma/meta/version.py
@@ -1,6 +1,6 @@
 class Version:
     major: tuple[int, str] = 7, "MAJOR"
     minor: tuple[int, str] = 2, "MINOR"
-    patch: tuple[int, str] = 0, "PATCH"
+    patch: tuple[int, str] = 1, "PATCH"
     full_version: str = f"{major[0]}.{minor[0]}.{patch[0]}"
     release: str = f"{major[0]}.{minor[0]}"
